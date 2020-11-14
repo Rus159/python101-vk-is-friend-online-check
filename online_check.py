1cd7ab9eaab9a0c4c1ec50dfd57026d55dbdf300
Rus159 <zabelin.tollyatti@yandex.ru>
2020-11-14 18:28:37 +0400

    move vk user id from file to .env

--- online_check.py
+++ online_check.py
@@ -7,14 +7,14 @@ load_dotenv()
 
 
 ACCESS_TOKEN = os.getenv('ACCESS_TOKEN')
-
+VK_USER_ID = os.getenv('VK_USER_ID')
 session = vk_api.VkApi(token=ACCESS_TOKEN)
 
 
 def check_online(prev_status, current_status):
     prev_status = current_status
     user_data = session.method('users.get',
-        {'user_ids': 221277111, 'fields': 'online'})
+        {'user_ids': VK_USER_ID, 'fields': 'online'})
     current_status = user_data[0].get('online')
     if not prev_status and current_status:
         sms.send_sms('{} {} онлайн'.format(user_data[0].get('first_name'),
